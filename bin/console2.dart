fc46df2d82c2030a1ddc306953bd7a2e559e7442
xem <xem.information@gmail.com>
2022-02-10 00:58:44 +0600

    In the name of Allah

---
+++ bin/console2.dart
@@ -0,0 +1,5 @@
+import 'package:console2/console2.dart' as console2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console2.calculate()}!');
+}
---
+++ test/console2_test.dart
@@ -0,0 +1,8 @@
+import 'package:console2/console2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
